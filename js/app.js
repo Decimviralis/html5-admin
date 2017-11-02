15e9da618ad9e37cc91455d8b7f29736bc76cb4b
maxim <decimviralis@gmail.com>
2017-11-03 00:17:49 +0600

    THIRDFIX

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-var testApp = angular.module('testApp', ["ngRoute"]);
-    testApp.config(function ($routeProvider) {
-        $routeProvider
-            .when('/',
+var testApp = angular.module('testApp', ["ngRoute"])
+    .config(function($routeProvider){
+        $routeProvider.when('/outside',
             {
-                templateUrl: 'views/FirstTest.html',
-                controller: 'FirstTestController'
-            })
-            .when('/charts',
-            {
-               templateUrl: 'views/SecondTest.html',
-               controller: 'SecondTestController'
+                templateUrl:'views/FirstTest.html',
+                controller:'FirstTestController'
             });
+        $routeProvider.when('/home',
+            {
+                templateUrl:'views/SecondTest.html',
+                controller:'SecondTestController'
+            });
+        $routeProvider.when('/form',
+            {
+                templateUrl: 'views/Form.html',
+                controller: 'FormController'
+            });
+        $routeProvider.otherwise({redirectTo: '/outside'});
     });
